84d7d0b3618742f86ce142f9881cf5506daddbf8
RafaelAmaralPaula <rafaelpaulajr@gmail.com>
2020-12-06 14:19:38 -0300

    Implementação das operações CRUD

--- spring-mvc-demo/src/main/java/com/springmvcdemo/controller/PessoaController.java
+++ spring-mvc-demo/src/main/java/com/springmvcdemo/controller/PessoaController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.springmvcdemo.model.Pessoa;
@@ -15,32 +17,61 @@ import com.springmvcdemo.repository.PessoaRepository;
 @Component
 @RequestMapping("/pessoas")
 public class PessoaController {
-		
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
-	
-	
+
 	@GetMapping("/listar")
-	public ModelAndView listar(){
+	public ModelAndView listar() {
 		ModelAndView view = new ModelAndView("listar.html");
 		List<Pessoa> pessoa = pessoaRepository.findAll();
-		
-		view.addObject("pessoas", pessoa );
-		
+
+		view.addObject("pessoas", pessoa);
+
 		return view;
 	}
-	
-	@GetMapping("/salvar")
-	public String paginaCadastro() {
-		return "cadastroPessoas.html";
+
+	@GetMapping("/paginaCadastro")
+	public ModelAndView paginaCadastro() {
+		ModelAndView view = new ModelAndView("cadastroPessoas.html");
+		view.addObject("pessoaobj", new Pessoa());
+		return view;
 	}
-	
-	
-	@PostMapping("/salvar")
+
+	@PostMapping("**/salvar")
 	public String salvar(Pessoa pessoa) {
-		 pessoaRepository.save(pessoa);
-		
+		pessoaRepository.save(pessoa);
+
+		return "redirect:/pessoas/listar";
+	}
+
+	@GetMapping("/editar/{codigo}")
+	public ModelAndView atualizar(@PathVariable Integer codigo) {
+		ModelAndView view = new ModelAndView("cadastroPessoas.html");
+
+		Pessoa pessoaEncontrada = pessoaRepository.getOne(codigo);
+
+		view.addObject("pessoaobj", pessoaEncontrada);
+
+		return view;
+
+	}
+
+	@GetMapping("/excluir/{codigo}")
+	public String excluir(@PathVariable Integer codigo) {
+
+		pessoaRepository.deleteById(codigo);
+
 		return "redirect:/pessoas/listar";
 	}
 	
+	@PostMapping("**/pesquisa")
+	public ModelAndView buscarPeloNome(@RequestParam("nome") String nome) {
+		ModelAndView view = new ModelAndView("listar.html");
+		
+		view.addObject("pessoas", pessoaRepository.findByNome(nome));
+		
+		return view;
+	}
+
 }
--- spring-mvc-demo/src/main/java/com/springmvcdemo/repository/PessoaRepository.java
+++ spring-mvc-demo/src/main/java/com/springmvcdemo/repository/PessoaRepository.java
@@ -1,11 +1,17 @@
 package com.springmvcdemo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.springmvcdemo.model.Pessoa;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
+
+	@Query("select p from Pessoa p where p.nome like %?1%")
+	List<Pessoa> findByNome(String nome);
 
 }
